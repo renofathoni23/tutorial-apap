5b64dcaea320776d4eb426684d258f2d9ba3bdd9
Reno Fathoni <renofathoni23@gmail.com>
2021-09-23 19:33:19 +0700

    add emsidi

---
+++ emsidi/src/main/java/apap/tutorial/emsidi/controller/PegawaiController.java
@@ -0,0 +1,89 @@
+package apap.tutorial.emsidi.controller;
+
+import apap.tutorial.emsidi.model.CabangModel;
+import apap.tutorial.emsidi.model.PegawaiModel;
+import apap.tutorial.emsidi.service.CabangService;
+import apap.tutorial.emsidi.service.PegawaiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class PegawaiController {
+    @Qualifier ("pegawaiServiceImpl")
+    @Autowired
+    PegawaiService pegawaiService;
+
+    @Qualifier("cabangServiceImpl")
+    @Autowired
+    CabangService cabangService;
+
+    @GetMapping("/pegawai/add/{noCabang}")
+    public String addPegawaiForm(@PathVariable Long noCabang, Model model){
+        PegawaiModel pegawai = new PegawaiModel();
+        CabangModel cabang = cabangService.getCabangByNoCabang(noCabang);
+        pegawai.setCabang(cabang);
+        model.addAttribute("noCabang", noCabang);
+        model.addAttribute("pegawai", pegawai);
+        return "form-add-pegawai";
+    }
+
+    @PostMapping("/pegawai/add")
+    public String addPegawaiSubmit(
+            @ModelAttribute PegawaiModel pegawai,
+            Model model
+    ){
+        pegawaiService.addPegawai(pegawai);
+        model.addAttribute("noCabang", pegawai.getCabang().getNoCabang());
+        model.addAttribute("namaPegawai", pegawai.getNamaPegawai());
+        return "add-pegawai";
+    }
+
+    @GetMapping("pegawai/update/{noPegawai}")
+    public String updatePegawaiForm(
+            @PathVariable Long noPegawai,
+            Model model
+    ){
+        PegawaiModel pegawai = pegawaiService.getPegawaiByNoPegawai(noPegawai);
+        model.addAttribute("pegawai",pegawai);
+        return "form-update-pegawai";
+    }
+
+    @PostMapping("pegawai/update")
+    public String updatePegawaiSubmit(
+            @ModelAttribute PegawaiModel pegawai,
+            Model model
+    ){
+        String updatePegawai = pegawaiService.updatePegawai(pegawai);
+        if(updatePegawai.equalsIgnoreCase("berhasil")){
+            model.addAttribute("msg","Berhasil mengupdate Pegawai");
+        }else{
+            model.addAttribute("msg", "Tidak berhasil mengupdate Pegawai");
+        }
+
+        return "update-pegawai";
+    }
+
+    @GetMapping("pegawai/delete/{noPegawai}")
+    public String deletePegawai(
+            @PathVariable Long noPegawai,
+            @ModelAttribute PegawaiModel Objpegawai,
+            Model model
+    ){
+        PegawaiModel pegawai = pegawaiService.getPegawaiByNoPegawai(noPegawai);
+        String deletePegawai = pegawaiService.deletePegawai(pegawai);
+
+        if(deletePegawai.equalsIgnoreCase("berhasil")){
+            model.addAttribute("msg","Berhasil delete Pegawai");
+        }else{
+            model.addAttribute("msg", "Tidak dapat delete Pegawai pada saat ini");
+        }
+
+        return "delete-pegawai";
+    }
+}
---
+++ emsidi/src/main/java/apap/tutorial/emsidi/repository/PegawaiDb.java
@@ -0,0 +1,12 @@
+package apap.tutorial.emsidi.repository;
+
+import apap.tutorial.emsidi.model.PegawaiModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PegawaiDb extends JpaRepository<PegawaiModel, Long>{
+    PegawaiModel findByNoPegawai(Long noPegawai);
+}
